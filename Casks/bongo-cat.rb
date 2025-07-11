783c69fac0da2a1cfcb5367e7a59d0c871fd1169
ayang <473033518@qq.com>
2025-07-11 12:09:41 +0800

    v0.6.2

--- Casks/bongo-cat.rb
+++ Casks/bongo-cat.rb
@@ -1,12 +1,12 @@
 cask "bongo-cat" do
-    version "0.6.1"
+    version "0.6.2"
   
     if Hardware::CPU.intel?
-      url "https://gh-proxy.com/github.com/ayangweb/BongoCat/releases/latest/download/BongoCat_x64.app.tar.gz"
-      sha256 "051125432deb49861d60987247719a9b8ff26ea1c864037cdc7243ffa42076b7"
+      url "https://github.com/ayangweb/BongoCat/releases/latest/download/BongoCat_x64.app.tar.gz"
+      sha256 "cdd53220ff0c3c987331c7dfdc034ddf36fab3096f0e03232ff36adeab4ec32f"
     else
-      url "https://gh-proxy.com/github.com/ayangweb/BongoCat/releases/latest/download/BongoCat_aarch64.app.tar.gz"
-      sha256 "64de4402e2ba49f958f37d814bafd3ced022fd95b32ed9f352b0dfa40cfd7920"
+      url "https://github.com/ayangweb/BongoCat/releases/latest/download/BongoCat_aarch64.app.tar.gz"
+      sha256 "c7a422a1ed7190ac1f684731aa3aefa49b5b51fac534efc4397a2750ce54047a"
     end
   
     name "BongoCat"
